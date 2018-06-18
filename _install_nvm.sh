f56f6d8d58581db9be0acd512803bd7bb36798cc
Roman M. Kos <roman.ko@inveritasoft.com>
2018-06-18 20:25:32 +0300

    Update nvm installation and preparation script

--- _install_nvm.sh
+++ _install_nvm.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
+if [ "$(command -v nvm)" != "" ]; then
+  echo 'Info: nvm is already installed.' >&2
+else
+  echo "Installing NVM"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+  echo "source ~/.bashrc"
+  source ~/.bashrc
+fi
+
+[ -x "$(command -v npm)" ] && [ -x "$(command -v node)" ] && nvmConfigured=1 || nvmConfigured=0
+
+if [ $nvmConfigured = 1 ]; then
+  echo 'Info: npm/node is already installed.' >&2
+else
+  echo "Using node LTS version"
+  nvm install --lts
+fi
+
 if [ -x "$(command -v npx)" ]; then
   echo 'Info: npx is already installed.' >&2
-  return
+else
+  echo "Installing NPX tooling globally"
+  npm install -g npx
 fi
 
-if [ -x "$(command -v npm)" ]; then
-  echo 'Info: npm is already installed.' >&2
-  return
+if [ -x "$(command -v yarn)" ]; then
+  echo 'Info: yarn is already installed.' >&2
+else
+  echo "Installing YARN tooling globally"
+  npm install -g yarn
 fi
-
-if [ -x "$(command -v node)" ]; then
-  echo 'Info: node is already installed.' >&2
-  return
-fi
-
-if [ -x "$(command -v nvm)" ]; then
-  echo 'Info: nvm is already installed.' >&2
-  return
-fi
-
-echo "Installing NVM"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-
-echo "source ~/.bashrc"
-source ~/.bashrc
-
-echo "Using node LTS version"
-nvm install --lts
-
-echo "Installing NPX tooling globally"
-npm install -g npx
