ffb84c568f58deee206b35d8328204d230e99d02
Roman M. Koss <roman.kos@inveritasoft.com>
2018-06-20 18:40:07 +0300

    Added missing GPG_TTY property

--- macos/_install_gpg.sh
+++ macos/_install_gpg.sh
@@ -4,3 +4,10 @@ else
     echo "Installing gpg"
     brew install gpg
 fi
+
+if [ "$(grep -nw ~/.bash_profile -e 'GPG_TTY')" != "" ]; then
+    echo "GPG_TTY configured"
+else
+    echo 'export GPG_TTY=$(tty) >> ~/.bash_profile'
+    echo 'export GPG_TTY=$(tty)' >> ~/.bash_profile
+fi
