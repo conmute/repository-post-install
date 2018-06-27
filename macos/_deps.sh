adb7dffee5de9b7a19f06f831efe2ea128aa29a2
Roman M. Koss <roman.kos@inveritasoft.com>
2018-06-27 17:39:15 +0300

    Impreved deps installation

--- macos/_deps.sh
+++ macos/_deps.sh
@@ -3,6 +3,10 @@
 if [ "$(whereis brew)" = "" ]; then
     echo "Installing brew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo 'brew tap caskroom/versions'
+    brew tap caskroom/versions
+    echo 'brew update'
+    brew update
 else
     echo "Brew already installed"
 fi
