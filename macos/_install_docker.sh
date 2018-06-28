13ab190246894a3592a5b96a7c39915d308c9cff
Roman M. Koss <roman.kos@inveritasoft.com>
2018-06-28 13:35:37 +0300

    Docker moved for future investigaion simlge Docker.dmg installation works way more better for MacOs

--- macos/_install_docker.sh
+++ macos/_install_docker.sh
@@ -4,9 +4,6 @@ if [ -x "$(command -v docker)" ]; then
     echo "Already installed: Docker"
 else
     echo "Installing docker"
-    brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
-
-    echo "Resolving docker-machine should be runed from root"
-    sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-    sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+    echo "The best option is to install Docker CE from official resource..."
+    echo "Automatic script will be implemented in future"
 fi
