0007097627f816106833df737bfbb3f33bf76ed3
Roman M. Koss <roman.kos@inveritasoft.com>
2018-07-17 15:25:52 +0300

    Added pyenv support

--- macos/_install_python3.sh
+++ macos/_install_python3.sh
@@ -5,4 +5,7 @@ if [ -x "$(command -v python3)" ]; then
 else
     echo "Installing Python3"
     brew install python3
+    brew install pyenv
+    echo 'export PATH="/Users/user/.pyenv:$PATH"' >> ~/.bash_profile
+    echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 fi
