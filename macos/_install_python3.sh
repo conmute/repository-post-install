1d8724fb71c70ae5e7b417849f1e7ccc0b8a390d
Roman M. Koss <roman.kos@inveritasoft.com>
2018-07-17 15:38:09 +0300

    Fix pyenv with set locale

--- macos/_install_python3.sh
+++ macos/_install_python3.sh
@@ -5,6 +5,13 @@ if [ -x "$(command -v python3)" ]; then
 else
     echo "Installing Python3"
     brew install python3
+
+    echo 'Set profile locale configuration. This will fix errors with local when using pyenv install'
+    echo 'export LC_ALL=en_US.UTF-8' >> ~/.bash_profile
+    echo 'export LANG=en_US.UTF-8' >> ~/.bash_profile
+    echo 'en_US.UTF-8 is set, if you need different change manually in ~/.bash_profile'
+
+    echo 'Installing PyEnv'
     brew install pyenv
     echo 'export PATH="/Users/user/.pyenv:$PATH"' >> ~/.bash_profile
     echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
